a751b797d5b5fd9485b2e759c67542460b3bc1a5
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2020-07-25 09:06:16 -0400

    add tinting by colour

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ An API that will generate easy MTG proxies for you.
 ```sh
 # setup and dev
 pipenv install
-FLASK_ENV=development pipenv run python ./app
+FLASK_ENV=development pipenv run python ./app/__main__.py
 ```
--- app/__main__.py
+++ app/__main__.py
@@ -1,11 +1,48 @@
 from flask import Flask, request, send_file
 from PIL import Image
 import numpy as np
+from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
+
+# import matplotlib.pyplot as plt
 import io
 
 app = Flask(__name__)
 frame = np.array(Image.open("./assets/frame.png"))
 powerbox = np.array(Image.open("./assets/powerbox.png"))
+color_mask = np.array(Image.open("./assets/color_mask.png"))[:, :, 0] == 255
+# plt.imshow(color_mask)
+# plt.show()
+
+
+def tint_image(image, color):
+    if color == "w":
+        return image
+    elif color == "u":
+        image[:, :, :3][color_mask] = image[:, :, :3][color_mask] * np.array(
+            [[[0.37, 0.32, 0.95]]]
+        )
+        return image
+    elif color == "b":
+        image[:, :, :3][color_mask] = image[:, :, :3][color_mask] * np.array(
+            [[[0.65, 0.13, 0.65]]]
+        )
+        return image
+    elif color == "r":
+        image[:, :, :3][color_mask] = image[:, :, :3][color_mask] * np.array(
+            [[[0.96, 0.41, 0.35]]]
+        )
+        return image
+    elif color == "g":
+        image[:, :, :3][color_mask] = image[:, :, :3][color_mask] * np.array(
+            [[[0.11, 0.60, 0.05]]]
+        )
+        return image
+    elif color == "c":
+        colors = rgb_to_hsv(image[:, :, :3])
+        colors[:, :, 1] = 0
+        image[:, :, :3][color_mask] = hsv_to_rgb[colors]
+        return image
+    return image
 
 
 @app.route("/card")
@@ -26,6 +63,7 @@ def hello():
         generated_image[-powerdims[0] :, -powerdims[1] :, -powerdims[2] :] = powerbox
 
     # tint the frame
+    tint_image(generated_image, color)
 
     # encode the response and add it
     img = Image.fromarray(generated_image, "RGBA")
